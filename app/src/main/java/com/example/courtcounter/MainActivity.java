28381270025ec05f17fe9cf28f8aacdc429414d9
Isiuaka Mohammed <mohammedelonx@gmail.com>
2019-12-26 06:11:40 +0100

    Added a reset button

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -45,6 +45,13 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamB(scoreTeamB);
     }
 
+    public void reset(View v){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
     public void displayForTeamA(int score){
         TextView scoreView = findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
